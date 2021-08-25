e8e1621dbe528ac37a7964aa493f5d5e3fa94424
Your Name <jasjohn3@uat.edu>
2021-08-24 22:11:29 -0700

    Added environment variables for security and protection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: 'JSPlasmo2@',
+    password: process.env.MYSQL_PASSWORD,
     database: 'election'
   },
 );
@@ -18,6 +19,62 @@ console.log('Connected to database');
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the API'
+  });
+});
+
+// GET a single candidate
+app.get('/api/candidate/:id',(req,res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({
+        error: err.message
+      });
+      return;
+    }
+    res.json({
+      message:'success',
+      data: row
+    });
+  });
+});
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
   });
 });
 
