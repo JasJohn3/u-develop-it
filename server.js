30d4e8e06d07b6e0b2233f64645814fa683a003d
Your Name <jasjohn3@uat.edu>
2021-08-24 17:50:21 -0700

    Connected to sql database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,7 +6,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'JSPlasmo2@',
+    database: 'election'
+  },
+);
+console.log('Connected to database');
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the API'
